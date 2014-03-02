5d094c8a463c42f421f9ee8c9f7d8ebaa3c75e7d
Haotian He <haotianh@uw.edu>
2014-03-01 22:20:06 -0800

    c

--- maxHeapify.java
+++ maxHeapify.java
@@ -3,6 +3,16 @@ import java.util.*;
 
 public class maxHeapify {
    public static void main(String[] args) {
+      // reads the number array first
+      System.out.println("Please input the numbers below:");
+      Scanner input = new Scanner(System.in);
+      ArrayList<Integer> numbers = new ArrayList<Integer>();
+      while (input.hasNextInt()) {
+         numbers.add(input.nextInt());
+      }
+      
+      // builds the binary tree
+      
       
    }
 }
