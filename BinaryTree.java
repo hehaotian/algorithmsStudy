c3d9473576d6bb2b0b99f91eca61977822cfdfb6
Haotian He <haotianh@uw.edu>
2014-03-02 00:06:35 -0800

    c

--- BinaryTree.java
+++ BinaryTree.java
@@ -18,6 +18,7 @@ public class BinaryTree {
       theTree.addNode(75, "Sale");
       theTree.addNode(85, "Medi");
       
+      // theTree.remove(25);
       theTree.inOrderTraverseTree(theTree.root);
    }
    
@@ -69,6 +70,88 @@ public class BinaryTree {
             }
          }
       }
+   }
+   
+   public Node findNode(int key) {
+      Node focusNode = root;
+      while (focusNode.key != key) {
+         if (key < focusNode.key) {
+            focusNode = focusNode.left;
+         } else {
+            focusNode = focusNode.right;
+         }
+         if (focusNode == null) return null; 
+      }
+      return focusNode;
+   }
+   
+   public boolean remove(int key) {
+      Node focusNode = root;
+      Node parent = root;
+      boolean isItALeftChild = true;
+      while (focusNode.key != key) {
+         parent = focusNode;
+         if (key < focusNode.key) {
+            isItALeftChild = true;
+            focusNode = focusNode.left;
+         } else {
+            isItALeftChild = false;
+            focusNode = focusNode.right;
+         }
+         if (focusNode == null) return false;
+      }
+      if (focusNode.left == null && focusNode.right == null) {
+         if (focusNode == root) {
+            root = null;
+         } else if (isItALeftChild) {
+            parent.left = null;
+         } else {
+            parent.right = null;
+         }
+      } else if (focusNode.right == null) {
+         if (focusNode == root) {
+            root = focusNode.left;
+         } else if (isItALeftChild) {
+            parent.left = focusNode.left;
+         } else {
+            parent.right = focusNode.right;
+         }
+      } else if (focusNode.left == null) {
+         if (focusNode == root) {
+            root = focusNode.right;
+         } else if (isItALeftChild) {
+            parent.left = focusNode.right;
+         } else {
+            parent.right = focusNode.left;
+         }
+      } else {
+         Node replacement = getReplacementNode(focusNode);
+         if (focusNode == root) {
+            root = replacement;
+         } else if (isItALeftChild) {
+            parent.left = replacement;
+         } else {
+            parent.right = replacement;
+         }
+         replacement.left = focusNode.left;
+      }
+      return true;
+   }
+   
+   public Node getReplacementNode(Node replacedNode) {
+      Node replacementParent = replacedNode;
+      Node replacement = replacedNode;
+      Node focusNode = replacedNode.right;
+      while (focusNode != null) {
+         replacementParent = replacement;
+         replacement = focusNode;
+         focusNode = focusNode.left;
+      }
+      if (replacement != replacedNode.right) {
+         replacementParent.left = replacement.right;
+         replacement.right = replacedNode.right;
+      }
+      return replacement;
    }
 }
 
---
+++ HeapSort.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class HeapSort {
+   
+   public static void main(String[] args) {
+   
+   }
+}
