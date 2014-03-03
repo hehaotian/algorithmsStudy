a5645dc887754ac8a445f0ada0b4bcddb4028d49
Haotian He <haotianh@uw.edu>
2014-03-02 19:14:36 -0800

    c

---
+++ reverseList.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class reverseList {
+
+   public static void main(String[] args) {
+      List myList = new List(1);
+      myList.next = new List(2);
+      myList.next.next = new List(3);
+      myList.next.next.next = new List(4);
+      System.out.println("Original List: " + myList.toString());
+      myList = Reverse(myList);
+      System.out.println("Reversed List: " + myList.toString());
+   }
+
+   static List Reverse(List L) {
+      if (L == null || L.next == null) {
+         return L;
+      }
+      List remainingReverse = Reverse(L.next);
+      L.next.next = L;
+      L.next = null;
+      return remainingReverse;
+   }
+}
+
+class List {
+   int value;
+   List next;
+   public List(int k) {
+      value = k;
+   }
+   public String toString() {
+      List current = this;
+      String output = "";
+      while (current != null) {
+         output += current.value + "->";
+         current = current.next;
+      }
+      return output + "NULL";
+   }
+}
